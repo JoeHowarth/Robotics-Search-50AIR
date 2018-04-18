50a98f71af91b73d53855f4a4dc752fb57892f75
Matthew Lee <22849218+mattrlee3@users.noreply.github.com>
2018-04-18 15:43:24 -0400

    Added coordinates.h
    Camera Positions

---
+++ coordinates.h
@@ -0,0 +1,19 @@
+#include <vector>
+
+
+struct Point {
+    float x;
+    float y;
+    string description;
+};
+
+std::vector<Point> camera_position = {
+        {-4.873, 1.701, "Top of Staircase"},
+        {-1.003, 2.135, "Front of Couches"},
+        {-0.448, 6.048, "Hallway Intersection"},
+        {-0.383, 8.614, "Front of Table"},
+        {-0.728, 13.86, "Elevator Intersection"},
+        {-7.463, 13.857, "End of Hallway"},
+        {4.998, 13.649, "Kitchen-EE Lab Hallway"},
+        {5.394, 6.621, "B. Tracey Hallway"}
+    };
